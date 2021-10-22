36c35b16e22c86c5b18d64db56ea6614c9bce11c
少侠 <sxqnwb@163.com>
2021-10-22 11:44:28 +0800

    vue2

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
-<!--
- * @Author: your name
- * @Date: 2021-03-31 15:54:18
- * @LastEditTime: 2021-04-01 11:18:03
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \hz-finances\src\App.vue
--->
 <template>
-  <router-view />
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
 
-})
-</script>
 <style lang="scss">
-@import "@/assets/style/utils.scss";
-@import "@/assets/style/base/reset.scss";
 #app {
-  height: 100%;
-  width: 100%;
-  color: #666;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-.spanColor {
-  padding-right: 10px;
-  color: #60a5f5;
-  cursor: pointer;
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
